9a87dcc0a213d33ea898fc49dd224a96e76d448d
Ankit Vadehra <ankitvad@gmail.com>
2025-09-09 21:50:42 -0400

    update

---
+++ New_Run/baseline_samp_1x.py
@@ -0,0 +1,235 @@
+'''
+Version 1 :
+- Manual Word Error Rate (WER) probability ratio for Out of Vocab (OOV) term substitutions.
+- The regular span substitution follows the true probability distribution observed from the ground-truth data.
+
+
+
+**python baseline_samp_1x.py ../../Data/LT/train_matrix_fd_man_aws.p LT/train/train ../../Data/LT/trgTrain.txt ../../Data/LT/srcTrain.txt
+
+
+**python baseline_samp_1x.py ../../Data/LT/train_matrix_fd_aws_man.p LT/dev/dev /home/avadehra/scribendi/dataAudio/parsed/LIB_TED/dev_trg.txt /home/avadehra/scribendi/dataAudio/parsed/LIB_TED/dev_src0.txt
+'''
+
+import pickle as p
+import json
+import subprocess
+import multiprocessing as mp
+from tqdm import tqdm
+import time
+import pandas as pd
+import numpy as np
+import re
+import gc
+from jiwer import wer
+from thefuzz import fuzz
+import random
+import sys
+from collections import Counter
+
+def closeMatch(word, freqDict):
+	#fdk = list(freqDict.keys())
+	ratioD = [[fuzz.ratio(word, i), i] for i in freqDict]
+	ratioD.sort(reverse=True)
+	return ratioD[0][1]
+
+
+def partition_text(x, fd, p_threshold):
+	if not type(x) is list:
+		x = x.split()
+	#OOV substititutions over the text - x
+	if OOV_correction:
+		x_new = []
+		for i in x:
+			if not i in fd:#OOV word
+				rndmSmp = random.random() <= werProb
+				if rndmSmp:
+					replWord = closeMatch(i, fd)
+				else:
+					replWord = i
+				x_new.append(replWord)
+			else:
+				x_new.append(i)
+		x_new = " ".join(x_new)
+		x_new = re.sub(r"\s+", " ", x_new).lstrip().rstrip()
+		x = x_new.split()
+	#Generate the partitions:
+	partitions = [[x[0]]]#first partition contains the first word.
+	if x[0] in fd:
+		prev_in = 1
+	else:
+		prev_in = 0
+	for i in range(1,len(x)):
+		w_i = x[i]
+		if prev_in == 0:
+			if w_i in fd:
+				prev_in = 1
+			else:
+				prev_in = 0
+			partitions.append([w_i])
+		else:
+			if not w_i in fd:
+				partitions.append([w_i])
+				prev_in = 0
+			else:
+				#prev_in = 1, w_i in fd
+				prev_frag = " ".join(partitions[-1]).lstrip().rstrip()
+				current_frag = prev_frag + " " + w_i
+				freq_prev = fd[prev_frag]
+				if not current_frag in fd:
+					freq_curr = 0
+				else:
+					freq_curr = fd[current_frag]
+				freq_phi = freq_curr/freq_prev
+				#assert(0.0 <= freq_phi <= 1.0), "Value : "+str(freq_phi)
+				if freq_phi >= p_threshold:#0.50:
+					#Part of the previous fragment:
+					partitions[-1].append(w_i)
+					#prev_in = 1
+				else:
+					partitions.append([w_i])
+				prev_in = 1
+	part_lens = []
+	for p in partitions:
+		#p = " ".join(p)
+		part_lens.append(len(p))
+	#part_lens = str(Counter(part_lens))
+	return partitions, part_lens
+
+def sample_replacement(frag, m):
+	#OOV - frag as it is right now:
+	if not frag in m:
+		return frag
+	else:
+		repl = []
+		repl_freq = []
+		for i in m[frag]:
+			repl.append(i)
+			repl_freq.append(m[frag][i][1])
+		tmp = random.choices(repl, repl_freq)[0]
+		return tmp
+
+
+
+def best_substitution(candidates):
+	"""
+	prefix_tokens : combined list of prefix tokens in the sentence (e.g., ["a","b","c","d","e"])
+	target_tokens   : list of tokens to be replaced. After prefix (e.g., ["f","g"])
+	candidates      : list of candidate replacements (each a string of tokens)
+	ngram_dict      : dict mapping ngram string -> count
+	max_n           : max ngram order available in dictionary
+	
+	The target to be edited is always at the END of the sentence.
+	"""
+	cand_pair = []
+	freq_pair = []
+	for c in candidates:
+		cand_pair.append(c.strip().split())
+		freq_pair.append(candidates[c][0])#freq
+	best_candidate = random.choices(cand_pair, freq_pair)[0]
+	return best_candidate#, best_score
+
+
+
+def get_corrpt_sent(p_thresh):
+	L = len(src_trg_pair)
+	srcSENTS = []
+	trgSENTS = []
+	editedSENTS = []
+	partition_lens = []
+	for i in tqdm(range(L)):
+		s = src_trg_pair[i][0].strip()
+		t = src_trg_pair[i][1].strip()
+		if s and t:
+			tmpEdit = []
+			iPart, p_len = partition_text(s, fdM, p_thresh)
+			for j in range(len(iPart)):
+				#Partition at a time
+				p = iPart[j]
+				p_str = " ".join(p).strip()
+				if not p_str in fdM:
+					tmpEdit += p
+				else:
+					p_cand_vals = mM[p_str]
+					p_cands = list(p_cand_vals.keys())
+					if (len(p_cands) == 1):
+						p_edit = p_cands[0].strip().split()
+					else:
+						p_edit = best_substitution(candidates = p_cand_vals)
+					#assert not p_edit is None
+					assert(p_edit != None)#, "prefix: "+ str(tmpEdit[-7:]) + " target: "+ str(p) + " cands: "+ str(p_cands)
+					tmpEdit += p_edit
+			if tmpEdit:
+				srcSENTS.append(s)
+				trgSENTS.append(t)
+				tmpEdit = " ".join(tmpEdit)
+				tmpEdit = re.sub(r"\s+"," ",tmpEdit).strip()
+				editedSENTS.append(tmpEdit)
+				partition_lens += p_len
+	#Write Out the Infornation and calculate the results of WER:
+	partition_lens = str(Counter(partition_lens))
+	thresh_val = str(int(p_thresh * 100))
+	x = open(opLoc+ "_" + edit_type + "_" + thresh_val + ".txt", "w")#xyz/td_dev_corrupt_05.txt
+	tmpwer = wer(trgSENTS, editedSENTS)
+	#print("WER Probability: ", werProb)
+	wO = x.write(str(round(tmpwer,6))+"\n\n")
+	wO = x.write(partition_lens+"\n\n")
+	#Printout the length of the source, target and edited sentences:
+	wO = x.write("Set Length: "+str(len(trgSENTS))+"\n")
+	x.close()
+
+
+
+
+if __name__ == '__main__':
+	'''
+	Script Input:
+	1 - Matrix Location
+	2 - Output Location + file type.
+	3 - Ground Truth Target
+	4 - Ground Truth Source
+	'''
+	mat_loc = sys.argv[1]#"sent_pair/train_matrix_fd_man_aws.p"
+	# if man_aws then type == "corrupt", if aws_man then type == "correct".
+	assert ("train_matrix_fd_" in mat_loc)
+	mat_edit = mat_loc.split("train_matrix_fd_")[-1].split(".")[0]
+	if mat_edit == "man_aws":#TRG -> SRC
+		edit_type = "corrupt"
+	elif mat_edit == "aws_man":#SRC -> TRG
+		edit_type = "correct"
+	else:
+		raise ValueError("Matrix name should contain either `man_aws` or `aws_man`")
+		sys.exit(1)
+	opLoc = sys.argv[2]#fLoc+"corrupt_en/"
+	assert("/" in opLoc)
+	#Load and read the Source and Target .txt file and calculate the WER.
+	gt_trg = sys.argv[3]#fLoc+"de_proc_transc.p"
+	gt_src = sys.argv[4]#fLoc+"en_proc_transc.p"
+	gt_trg = open(gt_trg, "r").read().strip().split("\n")
+	gt_src = open(gt_src, "r").read().strip().split("\n")
+	assert (len(gt_trg) == len(gt_src))
+	src_trg_pair = {}
+	for i in range(len(gt_trg)):
+		if edit_type == "corrupt":#man_aws = TRG -> SRC
+			src_trg_pair[i] = [gt_trg[i], gt_src[i]]
+		elif edit_type == "correct":#aws_man = SRC -> TRG
+			src_trg_pair[i] = [gt_src[i], gt_trg[i]]
+	tmp = p.load(open(mat_loc, "rb"))
+	mM = tmp[0]
+	fdM = tmp[1]
+	OOV_correction = True
+	werProb = wer(gt_trg, gt_src)
+	x = open(opLoc+ "_" + edit_type + "_orig.txt", "w")
+	print("WER Probability: ", werProb)
+	wO = x.write(str(round(werProb,6)))
+	x.close()
+	del gt_trg, gt_src
+	gc.collect()
+	prob_threshold = np.linspace(0, 1, 21)[1:-1]
+	#Non-multiprocessing the get_corrpt_sent function:
+	for p_thresh in tqdm(prob_threshold):
+		get_corrpt_sent(p_thresh)
+	#Multiprocessing the get_corrpt_sent function:
+	#cCount = mp.cpu_count() - 2
+	#with mp.Pool(cCount) as p:
+	#	r = list(tqdm(p.imap(get_corrpt_sent,prob_threshold), total = len(prob_threshold)))
